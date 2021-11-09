37beb7a25f06a6763f78a05f1ae742301352d85b
mfcrack <51809613+mfcrack@users.noreply.github.com>
2021-11-09 20:29:35 +0800

    Update Test1.java

--- src/main/java/Test1.java
+++ src/main/java/Test1.java
@@ -1,6 +1,11 @@
 public class Test1 {
+    
+    public void test99(){
+      System.out.println("111111")
+    }
     public static void main(String[] args) {
-        System.out.println("1");
+        System.out.println("2222222333333");
         System.out.println("新增了zhenyu的部分改了base程序");
+        System.out.println("又加了一点点")
     }
 }
